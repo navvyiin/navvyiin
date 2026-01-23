22487dad3b1c4751584816f938524a8b442326fd
Naval Kishore <usnavalg@gmail.com>
2026-01-23 12:10:10 +0530

    Time: 7 ms (47.15%), Space: 58.7 MB (54.71%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1 || s.length <= numRows) {
+        return s;
+    }
+
+    const rows = Array.from({ length: numRows }, () => "");
+    let currentRow = 0;
+    let goingDown = false;
+
+    for (let ch of s) {
+        rows[currentRow] += ch;
+
+        if (currentRow === 0 || currentRow === numRows - 1) {
+            goingDown = !goingDown;
+        }
+
+        currentRow += goingDown ? 1 : -1;
+    }
+
+    return rows.join("");
+};
