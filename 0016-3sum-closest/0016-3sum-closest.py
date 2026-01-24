6149a7ba1c0e39b9be93ee607b3a13172b1669ab
Naval Kishore <usnavalg@gmail.com>
2026-01-24 14:14:10 +0530

    Time: 371 ms (42.5%), Space: 19.4 MB (27.52%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -5,8 +5,7 @@ class Solution:
         closest = nums[0] + nums[1] + nums[2]
 
         for i in range(n - 2):
-            left = i + 1
-            right = n - 1
+            left, right = i + 1, n - 1
 
             while left < right:
                 current_sum = nums[i] + nums[left] + nums[right]
@@ -14,11 +13,11 @@ class Solution:
                 if abs(current_sum - target) < abs(closest - target):
                     closest = current_sum
 
-                if current_sum == target:
-                    return current_sum
-                elif current_sum < target:
+                if current_sum < target:
                     left += 1
-                else:
+                elif current_sum > target:
                     right -= 1
+                else:
+                    return target
 
         return closest
