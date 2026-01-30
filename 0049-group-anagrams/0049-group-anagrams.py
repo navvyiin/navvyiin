c206811bebd7b6ff594d937414b406e873462982
Naval Kishore <usnavalg@gmail.com>
2026-01-30 18:55:54 +0530

    Time: 11 ms (83.52%), Space: 23.8 MB (16.81%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs):
+        groups = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+            key = tuple(count)
+            groups[key].append(word)
+
+        return list(groups.values())
