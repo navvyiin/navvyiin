f13f1ba18d7166a18a473f1d2666f6240b87b114
Naval Kishore <usnavalg@gmail.com>
2026-01-30 18:56:53 +0530

    Time: 0 ms (100%), Space: 55.5 MB (9.06%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    let N = n;
+    if (N < 0) {
+        x = 1 / x;
+        N = -N;
+    }
+
+    let result = 1;
+
+    while (N > 0) {
+        if (N % 2 === 1) {
+            result *= x;
+        }
+        x *= x;
+        N = Math.floor(N / 2);
+    }
+
+    return result;
+};
