5fba5984e2f0a4a48f258f51b1f50c236da8b9d9
Naval Kishore <usnavalg@gmail.com>
2026-02-01 11:56:01 +0530

    Time: 0 ms (100%), Space: 43.4 MB (54.37%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        // Traverse from the least significant digit
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // If we reach here, all digits were 9
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+}
